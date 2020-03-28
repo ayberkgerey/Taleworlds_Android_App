85020c6309408f9f9750d82eb3a9986cd2d6fd98
Ayberk Gerey <ayberkgerey@gmail.com>
2020-03-29 02:33:55 +0300

    Add files via upload

--- app/src/main/java/com/geray/taleworlds/MainActivity.java
+++ app/src/main/java/com/geray/taleworlds/MainActivity.java
@@ -1,5 +1,6 @@
 package com.geray.taleworlds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawer;
 
@@ -21,12 +26,72 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MainFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_main);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_main:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MainFragment()).commit();
+                break;
+            case R.id.nav_games:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new GamesFragment()).commit();
+                break;
+            case R.id.nav_store:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new StoreFragment()).commit();
+                break;
+            case R.id.nav_company:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CompanyFragment()).commit();
+                break;
+            case R.id.nav_news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NewsFragment()).commit();
+                break;
+            case R.id.nav_careers:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CareersFragment()).commit();
+                break;
+            case R.id.nav_community:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CommunityFragment()).commit();
+                break;
+            case R.id.nav_contact:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+            case R.id.nav_login:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LoginFragment()).commit();
+                break;
+            case R.id.nav_register:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new RegisterFragment()).commit();
+                break;
+
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -37,6 +102,4 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
-
 }
